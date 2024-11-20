17d9d96eb3b48a1700eadb6326de0aa2e1ab0ecf
faiqueansari23 <ansarifaiqueakmal@gmail.com>
2024-11-20 22:25:24 +0530

    Initial commit
    Generated by react-native@0.75.4

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:50}}>BestProject</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
